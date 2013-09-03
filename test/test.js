c06b5199c405ecf98b978bc4de83ea6c523d4f1d
Shawn Miller <smiller@freshlogicstudios.com>
2013-09-03 08:44:24 -0500

    Convert tabs to spaces

--- test/test.js
+++ test/test.js
@@ -3,76 +3,76 @@ var whosit = require('../lib/whosit');
 
 describe('parse', function() {
     it('Shawn', function() {
-		var name = whosit.parse('Shawn');
-		assert.equal(name.first, 'Shawn');
-	});
+        var name = whosit.parse('Shawn');
+        assert.equal(name.first, 'Shawn');
+    });
 
-	it('Shawn Miller', function() {
-		var name = whosit.parse('Shawn Miller');
-		assert.equal(name.first, 'Shawn');
-		assert.equal(name.last, 'Miller');
-	});
+    it('Shawn Miller', function() {
+        var name = whosit.parse('Shawn Miller');
+        assert.equal(name.first, 'Shawn');
+        assert.equal(name.last, 'Miller');
+    });
 
-	it('Shawn Michael Miller', function() {
-		var name = whosit.parse('Shawn Michael Miller');
-		assert.equal(name.first, 'Shawn');
-		assert.equal(name.middle, 'Michael');
-		assert.equal(name.last, 'Miller');
-	});
+    it('Shawn Michael Miller', function() {
+        var name = whosit.parse('Shawn Michael Miller');
+        assert.equal(name.first, 'Shawn');
+        assert.equal(name.middle, 'Michael');
+        assert.equal(name.last, 'Miller');
+    });
 
-	it('Mr. Shawn Michael Miller', function() {
-		var name = whosit.parse('Mr. Shawn Michael Miller');
-		assert.equal(name.salutation, 'Mr.');
-		assert.equal(name.first, 'Shawn');
-		assert.equal(name.middle, 'Michael');
-		assert.equal(name.last, 'Miller');
-	});
+    it('Mr. Shawn Michael Miller', function() {
+        var name = whosit.parse('Mr. Shawn Michael Miller');
+        assert.equal(name.salutation, 'Mr.');
+        assert.equal(name.first, 'Shawn');
+        assert.equal(name.middle, 'Michael');
+        assert.equal(name.last, 'Miller');
+    });
 
-	it('Mr. Shawn Michael Miller Sr.', function() {
-		var name = whosit.parse('Mr. Shawn Michael Miller Sr.');
-		assert.equal(name.salutation, 'Mr.');
-		assert.equal(name.first, 'Shawn');
-		assert.equal(name.middle, 'Michael');
-		assert.equal(name.last, 'Miller');
-		assert.equal(name.suffix, 'Sr.');
-	});
+    it('Mr. Shawn Michael Miller Sr.', function() {
+        var name = whosit.parse('Mr. Shawn Michael Miller Sr.');
+        assert.equal(name.salutation, 'Mr.');
+        assert.equal(name.first, 'Shawn');
+        assert.equal(name.middle, 'Michael');
+        assert.equal(name.last, 'Miller');
+        assert.equal(name.suffix, 'Sr.');
+    });
 
-	it('Mr. Shawn Michael Miller Sr.', function() {
-		var name = whosit.parse('Mr. Shawn Michael Miller Sr.');
-		assert.equal(name.salutation, 'Mr.');
-		assert.equal(name.first, 'Shawn');
-		assert.equal(name.middle, 'Michael');
-		assert.equal(name.last, 'Miller');
-		assert.equal(name.suffix, 'Sr.');
-	});
+    it('Mr. Shawn Michael Miller Sr.', function() {
+        var name = whosit.parse('Mr. Shawn Michael Miller Sr.');
+        assert.equal(name.salutation, 'Mr.');
+        assert.equal(name.first, 'Shawn');
+        assert.equal(name.middle, 'Michael');
+        assert.equal(name.last, 'Miller');
+        assert.equal(name.suffix, 'Sr.');
+    });
 
-	it('Mister Rogers', function() {
-		var name = whosit.parse('Mister Rogers');
-		assert.equal(name.salutation, 'Mister');
-		assert.equal(name.last, 'Rogers');
-	});
+    it('Mister Rogers', function() {
+        var name = whosit.parse('Mister Rogers');
+        assert.equal(name.salutation, 'Mister');
+        assert.equal(name.last, 'Rogers');
+    });
 
-	it('Doctor Who', function() {
-		var name = whosit.parse('Doctor Who');
-		assert.equal(name.salutation, 'Doctor');
-		assert.equal(name.last, 'Who');
-	});
+    it('Doctor Who', function() {
+        var name = whosit.parse('Doctor Who');
+        assert.equal(name.salutation, 'Doctor');
+        assert.equal(name.last, 'Who');
+    });
 
-	it('Dr. Dre', function() {
-		var name = whosit.parse('Dr. Dre');
-		assert.equal(name.salutation, 'Dr.');
-		assert.equal(name.last, 'Dre');
-	});
+    it('Dr. Dre', function() {
+        var name = whosit.parse('Dr. Dre');
+        assert.equal(name.salutation, 'Dr.');
+        assert.equal(name.last, 'Dre');
+    });
 
-	it('Prof. Dre', function() {
-		var name = whosit.parse('Prof. Plum');
-		assert.equal(name.salutation, 'Prof.');
-		assert.equal(name.last, 'Plum');
-	});
+    it('Prof. Dre', function() {
+        var name = whosit.parse('Prof. Plum');
+        assert.equal(name.salutation, 'Prof.');
+        assert.equal(name.last, 'Plum');
+    });
 
-	it('Professor Plum', function() {
-		var name = whosit.parse('Professor Plum');
-		assert.equal(name.salutation, 'Professor');
-		assert.equal(name.last, 'Plum');
-	});
+    it('Professor Plum', function() {
+        var name = whosit.parse('Professor Plum');
+        assert.equal(name.salutation, 'Professor');
+        assert.equal(name.last, 'Plum');
+    });
 });
